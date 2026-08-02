8291d011244f549d801a82d97c0d4d945a5a107e
yousef mousa1 <mousayoussef4@gmail.com>
2026-08-02 21:46:17 +0300

    Restructure project into backend/frontend folders

--- backend/src/middlewares/rateLimiter.js
+++ backend/src/middlewares/rateLimiter.js
@@ -1,7 +1,6 @@
 const rateLimit = require("express-rate-limit");
 
-// حد عام لكل المشروع: 100 طلب كل 15 دقيقة لكل IP
-// مناسب لمستخدم بيتصفح المنتجات والكاتيجوريز عادي
+//  مناسب لمستخدم بيتصفح المنتجات والكاتيجوريز عادي
 const generalLimiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 دقيقة
   max: 10,
@@ -9,12 +8,11 @@ const generalLimiter = rateLimit({
     success: false,
     message: "Too many requests, please try again later",
   },
-  standardHeaders: true, // يرجع معلومات الحد في response headers (RateLimit-*)
-  legacyHeaders: false, // يوقف الـ headers القديمة (X-RateLimit-*)
+  standardHeaders: true,
+  legacyHeaders: false,
 });
 
-// حد أشد جداً لصفحات الـ Auth الحساسة: 5 محاولات كل 15 دقيقة لكل IP
-// حماية من محاولات تخمين الباسورد (brute force)
+//  حماية من محاولات تخمين الباسورد (brute force)
 const authLimiter = rateLimit({
   windowMs: 15 * 60 * 1000,
   max: 5,
