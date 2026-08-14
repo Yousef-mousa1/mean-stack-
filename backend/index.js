efcdebbf1538ba57fe84dc6396b4d1d30836e62f
yousef mousa1 <mousayoussef4@gmail.com>
2026-08-14 05:29:48 +0300

    add vercel config and update index.js

--- backend/index.js
+++ backend/index.js
@@ -4,10 +4,15 @@ dotenv.config();
 const app = require("./src/app");
 const connectDB = require("./src/config/db");
 
+// الاتصال بقاعدة البيانات
 connectDB();
 
-const PORT = process.env.PORT || 3000;
+// تشغيل السيرفر محلياً فقط أثناء التطوير Local Development
+if (process.env.NODE_ENV !== 'production') {
+  const PORT = process.env.PORT || 3000;
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+}
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+module.exports = app;
